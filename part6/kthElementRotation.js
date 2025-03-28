754aacd995be98ca06758a63eee3293a97f4948e
Kanak Ramapure <kanak1803@gmail.com>
2025-03-28 11:20:42 +0530

    Update kthElementRotation.js

--- part6/kthElementRotation.js
+++ part6/kthElementRotation.js
@@ -1,6 +1,11 @@
-let k = 2;
+let k = 4;
 let arr = [1, 2, 3, 4, 5];
 let length = arr.length;
+
+if ((k === length)) {
+  console.log(arr);
+  return;
+}
 
 for (let j = 0; j < k; j++) {
   let copy = arr[0];
