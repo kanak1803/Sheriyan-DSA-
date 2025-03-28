d4cb2191b3e96dd20960acbfaed4a76d4f2bf268
Kanak Ramapure <kanak1803@gmail.com>
2025-03-28 11:45:05 +0530

    Update OptimizeKthElementRotation.js

--- part6/OptimizeKthElementRotation.js
+++ part6/OptimizeKthElementRotation.js
@@ -1,10 +1,11 @@
 let arr = [1, 2, 3, 4, 5];
 let length = arr.length;
 let temp = new Array(length);
-let k = 2;
+let k = 1;
 
 for (let i = 0; i < length; i++) {
-  temp[i] = arr[(i + k) % length];
+//   temp[i] = arr[(i + k) % length]; //left roatation
+  temp[(i + k) % length] = arr[i]; //right roatation
 }
 
 console.log(arr);
