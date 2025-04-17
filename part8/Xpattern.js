7f4376c91e5f0c6ef564c40466423099f7efe59b
Kanak Ramapure <kanak1803@gmail.com>
2025-04-17 11:07:20 +0530

    Create Xpattern.js

---
+++ part8/Xpattern.js
@@ -0,0 +1,10 @@
+for (let row = 1; row <= 5; row++) {
+  for (let col = 0; col <= 5; col++) {
+    if (row + col == 5 + 1 || row == col) {
+      process.stdout.write("*");
+    } else {
+      process.stdout.write(" ");
+    }
+  }
+  console.log();
+}
