7812cd2eb7caf7ee4c8a0114ce665cfc9718f74c
Kanak Ramapure <kanak1803@gmail.com>
2025-04-17 11:09:55 +0530

    Update Xpattern.js

--- part8/Xpattern.js
+++ part8/Xpattern.js
@@ -1,9 +1,9 @@
 for (let row = 1; row <= 5; row++) {
   for (let col = 0; col <= 5; col++) {
     if (row + col == 5 + 1 || row == col) {
-      process.stdout.write("*");
+      process.stdout.write("* ");
     } else {
-      process.stdout.write(" ");
+      process.stdout.write("  ");
     }
   }
   console.log();
