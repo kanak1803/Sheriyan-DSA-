3bb37ace3388be1f3765ece8a31643a329ee70ef
Kanak Ramapure <kanak1803@gmail.com>
2025-04-17 10:46:37 +0530

    mirror

---
+++ part8/mirrorTriangle.js
@@ -0,0 +1,11 @@
+for (let row = 1; row <= 5; row++) {
+  for (let col = 1; col <= 5 - row; col++) {
+    process.stdout.write(" ");
+  }
+
+  for (let col = 1; col <= row; col++) {
+    process.stdout.write("*");
+  }
+
+  console.log();
+}
