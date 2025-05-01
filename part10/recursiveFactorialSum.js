286fce252abfe93b84ab826e87a76d8d9819163a
Kanak Ramapure <kanak1803@gmail.com>
2025-05-01 22:46:36 +0530

    Update recursiveFactorialSum.js

--- part10/recursiveFactorialSum.js
+++ part10/recursiveFactorialSum.js
@@ -1,7 +1,7 @@
-function fibboSum(n) {
+function fibbonacciSum(n) {
   if (n == 0) return 0;
   if (n == 1) return 1;
-  return fibboSum(n - 1) + fibboSum(n - 2);
+  return fibbonacciSum(n - 1) + fibbonacciSum(n - 2);
 }
 
-console.log(fibboSum(5));
+console.log(fibbonacciSum(5));
