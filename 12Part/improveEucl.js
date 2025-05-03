8b8de86fca66f63a0824178083d9a9c6a2f4ad4a
Kanak Ramapure <kanak1803@gmail.com>
2025-05-03 22:26:20 +0530

    Update improveEucl.js

--- 12Part/improveEucl.js
+++ 12Part/improveEucl.js
@@ -1,9 +1,16 @@
 
 
-function improveGCD(a,b){
-    if(b ==0 ) return a
+// function improveGCD(a,b){
+//     if(b ==0 ) return a
 
-    return improveGCD(b,a%b)
+//     return improveGCD(b,a%b)
+// }
+
+// console.log(improveGCD(16,14))
+
+let a = 16,b=14
+
+for (let i = a; i < array.length; i++) {
+    const element = array[i];
+    
 }
-
-console.log(improveGCD(16,14))
