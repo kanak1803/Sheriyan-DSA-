b5175ad48cb94b7260f24b702d2272d7551be7e0
Kanak Ramapure <kanak1803@gmail.com>
2025-05-08 11:44:42 +0530

    Create BS.js

---
+++ 12_part/BS.js
@@ -0,0 +1,16 @@
+let arr = [2, 5, 8, 9, 12, 18, 20, 25, 67, 101, 125];
+let value = 101;
+
+function BS(arr, start, end, target) {
+  if (start > end) return -1;   
+  let mid = Math.floor((start + end) / 2);
+  
+  if (target === arr[mid]) return mid;
+  if (target > arr[mid]) {
+   return BS(arr, mid + 1, end,target);
+  } else {
+   return BS(arr, start, mid - 1,target);
+  }
+}
+
+console.log(BS(arr, 1, arr.length - 1, value));
