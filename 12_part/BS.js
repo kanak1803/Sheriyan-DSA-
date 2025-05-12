af3eca8e3f155a93ad5e81545173fa1f52bfe067
Kanak Ramapure <kanak1803@gmail.com>
2025-05-12 13:04:12 +0530

    Update BS.js

--- 12_part/BS.js
+++ 12_part/BS.js
@@ -1,16 +1,16 @@
 let arr = [2, 5, 8, 9, 12, 18, 20, 25, 67, 101, 125];
 let value = 101;
 
-function BS(arr, start, end, target) {
+function binarySearchRecursive(arr, start, end, target) {
   if (start > end) return -1;   
   let mid = Math.floor((start + end) / 2);
   
   if (target === arr[mid]) return mid;
   if (target > arr[mid]) {
-   return BS(arr, mid + 1, end,target);
+   return binarySearchRecursive(arr, mid + 1, end,target);
   } else {
-   return BS(arr, start, mid - 1,target);
+   return binarySearchRecursive(arr, start, mid - 1,target);
   }
 }
 
-console.log(BS(arr, 1, arr.length - 1, value));
+console.log(binarySearchRecursive(arr, 1, arr.length - 1, value));
